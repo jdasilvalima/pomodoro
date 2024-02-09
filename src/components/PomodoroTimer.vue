2ff18abf445f7ed37e842b8830d59020f3785ea1
jade <jade.dasilva.lima.jds@gmail.com>
2024-02-09 02:54:32 -0500

    skeleton pomodoro

---
+++ src/components/PomodoroTimer.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref, computed, onUnmounted } from 'vue';
+
+//setInterval
+//onUnmounted(() => clearInterval(interval));
+</script>
+
+<template>
+  25 : 00
+</template>
