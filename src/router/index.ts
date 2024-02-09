e117f0f849a1b91fdf77bcc51bb650fee3f43090
jade <jade.dasilva.lima.jds@gmail.com>
2024-02-09 02:58:26 -0500

    build correction

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,7 +20,7 @@ const router = createRouter({
     {
       path: '/modern',
       name: 'modern',
-      component: () => import('@/views/ModernPomodoroView.vue)
+      component: () => import('@/views/ModernPomodoroView.vue')
     }
   ]
 })
---
+++ src/views/ModernPomodoroView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import PomodoroTimer from '@/components/PomodoroTimer.vue';
+</script>
+
+<template>
+  <PomodoroTimer/>
+</template>
